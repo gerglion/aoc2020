a6068066b4856f871f1f7c8d9717dacb5bf5608d
Morin <gmorin@its-x-4d8jhd2.ur.rochester.edu>
2020-12-04 14:44:38 -0500

    Day 1

---
+++ day01/solution.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+
+import sys
+
+def main():
+    # test
+    if len(sys.argv) > 1:
+        input_file = sys.argv[1]
+    else:
+        input_file = 'inputtest.txt'
+
+    #Solve Part 1
+
+
+    #Solve Part 2
+
+if __name__ == '__main__':
+    main()
